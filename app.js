40d63169a67e62f01d564fd938d7d806cdc3f6e7
Jonathan Funes <jonathan.funes@itrocksweb.com>
2024-11-17 16:45:17 -0300

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
@@ -14,6 +15,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors())
 
 // Swagger middleware
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
